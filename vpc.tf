2948b6703bcc4121704c5561696da001f666e621
dinesh <dinesh@gmail.com>
2023-04-08 11:02:17 +0530

    removed some pvt resource

--- vpc.tf
+++ vpc.tf
@@ -37,27 +37,6 @@ resource "aws_subnet" "pubsub2" {
     Name = "PUB-SUB-2"
   }
 }
-#PVT subnet 1
-resource "aws_subnet" "pvtsub1" {
-  vpc_id            = aws_vpc.myvpc.id
-  cidr_block        = var.subpvt1
-  availability_zone = var.az_sub_pvt1
-
-  tags = {
-    Name = "PVT-SUB-1"
-  }
-}
-#PVT subnet 2
-resource "aws_subnet" "pvtsub2" {
-  vpc_id            = aws_vpc.myvpc.id
-  cidr_block        = var.subpvt2
-  availability_zone = var.az_sub_pvt2
-
-  tags = {
-    Name = "PVT-SUB-2"
-  }
-}
-#Route Table
 
 # PUB-RT-1
 resource "aws_route_table" "pubrt1" {
@@ -89,32 +68,6 @@ resource "aws_route_table" "pubrt2" {
 
 #PVT-RT-1
 
-resource "aws_route_table" "pvtrt1" {
-  vpc_id = aws_vpc.myvpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.pvnat1.id
-  }
-
-  tags = {
-    Name = "RT-PVTRT-1"
-  }
-}
-#PVT-RT-2
-resource "aws_route_table" "pvtrt2" {
-  vpc_id = aws_vpc.myvpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.pvnat2.id
-  }
-
-    tags = {
-      Name = "RT-PVTRT-2"
-    }
-  
-}
 #Route table Asc
 #ASCPUB SUB 1
 resource "aws_route_table_association" "pubasc1" {
@@ -126,124 +79,4 @@ resource "aws_route_table_association" "pubasc2" {
   subnet_id      = aws_subnet.pubsub2.id
   route_table_id = aws_route_table.pubrt2.id
 }
-#ASCPVT SUB 1
-resource "aws_route_table_association" "pvtasc1" {
-  subnet_id      = aws_subnet.pvtsub1.id
-  route_table_id = aws_route_table.pvtrt1.id
-}
-#ASCPVT SUB 1
-resource "aws_route_table_association" "pvtasc2" {
-  subnet_id      = aws_subnet.pvtsub2.id
-  route_table_id = aws_route_table.pvtrt2.id
-}
-#EIP-1
-resource "aws_eip" "eip1" {
 
-  vpc = true
-}
-#EIP-1
-resource "aws_eip" "eip2" {
-
-  vpc = true
-}
-#NAT-1
-resource "aws_nat_gateway" "pvnat1" {
-  allocation_id = aws_eip.eip1.id
-  subnet_id     = aws_subnet.pubsub1.id
-
-  tags = {
-    Name = "T-NAT1"
-  }
-}
-#NAT-1
-resource "aws_nat_gateway" "pvnat2" {
-  allocation_id = aws_eip.eip2.id
-  subnet_id     = aws_subnet.pubsub2.id
-
-  tags = {
-    Name = "T-NAT2"
-  }
-}
-
-#SEG
-resource "aws_security_group" "Pub_Sec" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.myvpc.id
-
-  ingress {
-    description = "https"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "ssh"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "http"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "PUB-SEC"
-  }
-}
-
-resource "aws_security_group" "Pvt_Sec" {
-  name        = "allow_tls-pvt"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.myvpc.id
-
-  ingress {
-    description     = "https"
-    from_port       = 443
-    to_port         = 443
-    protocol        = "tcp"
-    security_groups = [aws_security_group.Pub_Sec.id]
-  }
-
-  ingress {
-    description     = "ssh"
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = [aws_security_group.Pub_Sec.id]
-  }
-  ingress {
-    description     = "RDS"
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = [aws_security_group.Pub_Sec.id]
-  }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    security_groups = [aws_security_group.Pub_Sec.id]
-
-  }
-
-  tags = {
-    Name = "PVT-SEC"
-  }
-}
